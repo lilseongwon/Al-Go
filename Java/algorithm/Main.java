cb3d0f30ded977b621a2ad1c03f02c2254f7e546
김성원 <102791105+lilseongwon@users.noreply.github.com>
2022-10-23 14:00:20 +0900

    ✅ :: 1271
    BigInt에 대해서 알 수 있었다

---
+++ Java/algorithm/Main.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        scanner.close();
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
+    }
+}
