b6479648d95e70dedc2e3290af725dbb69960a96
김성원 <102791105+lilseongwon@users.noreply.github.com>
2022-10-24 17:15:21 +0900

    ✅ :: 3003

---
+++ Java/algorithm/Main4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a1 = 1;
+        int b1 = 1;
+        int c1 = 2;
+        int d1 = 2;
+        int e1 = 2;
+        int f1 = 8;
+
+        int a = a1 - scanner.nextInt();
+        int b = b1 - scanner.nextInt();
+        int c = c1 - scanner.nextInt();
+        int d = d1 - scanner.nextInt();
+        int e = e1 - scanner.nextInt();
+        int f = f1 - scanner.nextInt();
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+    }
+}
