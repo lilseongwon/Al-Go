54781d1e4b5932d15fff72044d7a43feeb61cfaa
김성원 <102791105+lilseongwon@users.noreply.github.com>
2023-04-02 19:46:26 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,6 +1,18 @@
 def solution(food):
-    answer = ''
-    for i, f in enumerate(food[1:]):
-        answer += str(i+1) * (f//2)
-    return answer + '0' + answer[::-1] 
+    result = ''
+    first = ''
+    for a, i in enumerate(food):
+        food[a] = int(i / 2)
+
+    for k, v in enumerate(food):
+        if(k != 0):
+            for j in range(v):
+                first += str(k)
+                
+    result += first
+    result += '0'
     
+    for i in reversed(first):
+        result += i
+    return result
+        
